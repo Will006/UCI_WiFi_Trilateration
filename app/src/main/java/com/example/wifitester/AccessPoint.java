42608933f773d8c1126769d5c431e0c8ffbd4102
Will006 <williampickeringiii@gmail.com>
2020-10-24 16:21:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/wifitester/AccessPoint.java
@@ -0,0 +1,35 @@
+package com.example.wifitester;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.*;
+
+class AccessPoint
+{
+    static String AP_Extension = "_TrilaterationAP";
+    String SSID;
+    int Location[];
+
+    AccessPoint(String SSID_In, int Location_In[])
+    {
+        SSID=SSID_In;
+        Location = Location_In;
+    }
+    static private HashMap<String, AccessPoint> AP_DataBase = new HashMap<String, AccessPoint>() {{
+        put("BIO251_A"+AP_Extension, new AccessPoint("BIO251_A"+AP_Extension, new int[]{0, 10, 0}));
+        put("BIO251_B"+AP_Extension, new AccessPoint("BIO251_A"+AP_Extension, new int[]{0, 10, 0}));
+    }};
+
+    static public AccessPoint GetAccessPoint(String SSID_In)
+    {
+        return(AP_DataBase.get((SSID_In)));
+    }
+
+    
+/*
+    static HashMap<String, AccessPoint> AP_DataBase = {
+            new AccessPoint("BIO251_A"+AP_Extension, new int[]{0, 10, 0}),
+            new AccessPoint("BIO251_B"+AP_Extension, new int[]{40, 10, 0})
+    };*/
+}
---
+++ app/src/main/java/com/example/wifitester/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.wifitester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    //AccessPoint test =  new AccessPoint("BIO251_A"+AccessPoint.AP_Extension, new int[]{0, 10, 0});
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private int size = 0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.ScanButton);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
+
+        listView = findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "WiFi is disabled ... We need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        scanWifi();
+    }
+    //https://developer.android.com/reference/android/net/wifi/WifiManager
+    //https://developer.android.com/reference/android/net/ConnectivityManager
+    /*
+    https://stackoverflow.com/questions/16485370/wifi-position-triangulation
+    https://developer.android.com/reference/android/net/wifi/WifiManager#EXTRA_NEW_RSSI
+    https://developer.android.com/reference/android/net/wifi/WifiManager#RSSI_CHANGED_ACTION
+
+     */
+    private void scanWifi() {
+        arrayList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver()
+    {
+
+        List<AccessPoint> Visable_APs = new LinkedList<>();
+        AccessPoint temp_AP;
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+            for (ScanResult scanResult : results)
+            {
+                temp_AP = AccessPoint.GetAccessPoint(scanResult.SSID);
+                if(temp_AP!=null)
+                {
+                    Visable_APs.add(temp_AP);
+                    arrayList.add(scanResult.SSID + ": dB[" + scanResult.level+"]");
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        }
+    };
+}
